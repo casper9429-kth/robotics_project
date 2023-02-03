12f5860b7679ea70cfd3e22522995ad72ee8796d
casper9429 <casper9429@gmail.com>
2023-02-03 14:35:09 +0100

    arm_camera node and launch file

---
+++ arm_camera/src/arm_camera.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+import rospy
+import cv2
+import numpy as np
+from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import Image
+
+
+class CameraNode:
+    def __init__(self):
+        # Initialize the node
+        self.node_name = "arm_camera"
+        self.bridge = CvBridge()
+        self.image_pub = rospy.Publisher("/camera/image", Image, queue_size=1)
+        
+
+    def start(self):
+        rospy.init_node(self.node_name)
+        cap = cv2.VideoCapture(0)
+        rate = rospy.Rate(10)
+        while not rospy.is_shutdown():
+            rospy.loginfo("Publishing image")
+            ret, frame = cap.read()
+            if ret:
+                try:
+                    # 
+                    
+                    image_message = self.bridge.cv2_to_imgmsg(frame, "bgr8")
+                    self.image_pub.publish(image_message)
+                except CvBridgeError as e:
+                    print(e)
+            rate.sleep()
+
+if __name__ == "__main__":
+    rospy.loginfo("Starting camera node")
+    try:
+
+        node = CameraNode()
+        node.start()
+    except rospy.ROSInterruptException:
+        rospy.loginfo("Camera node terminated.")
