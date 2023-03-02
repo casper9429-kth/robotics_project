ac64e6935239cc823be538172e0f35a862128f41
= <=>
2023-03-02 17:45:25 +0100

    debut postition

--- detection/src/object_classification.py
+++ detection/src/object_classification.py
@@ -2,7 +2,8 @@
 import rospy
 
 from typing import Dict, List
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CameraInfo
+from geometry_msgs.msg import Point
 
 import matplotlib.patches as patches
 import matplotlib.pyplot as plt
@@ -26,9 +27,12 @@ class Object_classifier():
         # Subscribers 
         # Or use compressed image?
         self.sub_image = rospy.Subscriber("/camera/color/image_raw", Image, self.image_callback) 
+        self.sub_depth= rospy.Subscriber("/camera/aligned_depth_to_color/image_raw", Image, self.depth_image_callback)
+        self.sub_camera_info= rospy.Subscriber("/camera/color/camera_info", CameraInfo, self.camera_info_callback)  
         
         # Publisher
         self.bb_pub = rospy.Publisher("/detection/bounding_boxes", BoundingBox, queue_size=10)
+        self.image_bb_pub = rospy.Publisher("/detection/image_with_bounding_boxes", Image, queue_size=10)
 
          # Define rate
         self.update_rate = 10 # [Hz] Change this to the rate you want
@@ -47,7 +51,10 @@ class Object_classifier():
         self.bridge = CvBridge()
 
         self.mapping = ["Binky", "Hugo", "Slush", "Muddles", "Kiki", "Oakie", "Cube", "Sphere"]
-        #self.count = 0
+        self.depth = None
+        
+        self.camera_info = [None, None, None, None]
+
         
         
 
@@ -60,13 +67,38 @@ class Object_classifier():
             cv_image = self.bridge.imgmsg_to_cv2(msg, "rgb8")
             np_arr = np.asarray(cv_image)
             im = pil_image.fromarray(np_arr)
-            self.compute_bb(im, msg.header.stamp, msg.header.frame_id)
+            self.compute_bb(im, msg.header.stamp, msg.header.frame_id, self.depth, cv_image) 
+
+        except CvBridgeError as e:
+            print(e)
+            
+    def depth_image_callback(self, msg): 
+        """Callback function for the topic"""
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(msg, "16UC1")
+            np_arr = np.asarray(cv_image)
+            self.depth = np_arr
             
 
         except CvBridgeError as e:
             print(e)
 
-
+    def camera_info_callback(self, msg): 
+        """Callback function for the topic"""
+        
+        self.camera_info[0] = msg.K[0] #fx
+        self.camera_info[1] = msg.K[4] #fy
+        self.camera_info[2] = msg.K[2] #cx
+        self.camera_info[3] = msg.K[5] #cy
+        
+        
+        # **Intrinsic camera matrix for the raw (distorted) images.**
+            #      [fx  0 cx]
+            #  K = [ 0 fy cy]
+            #      [ 0  0  1]
+            #  Projects 3D points in the camera coordinate frame to 2D pixel
+            #  coordinates using the focal lengths (fx, fy) and principal point
+            # (cx, cy).
 
     def load_model(self,model: torch.nn.Module, path: str, device: str) -> torch.nn.Module:
         """Load model weights from disk.
@@ -85,7 +117,7 @@ class Object_classifier():
 
 
 
-    def compute_bb(self,image, stamp, frame_id):     
+    def compute_bb(self,image, stamp, frame_id, depth_image, cv_image):     
         
         test_images = []
         torch_image, _  = self.detector.input_transform(image, [])
@@ -96,33 +128,65 @@ class Object_classifier():
         with torch.no_grad():
             out = self.detector(test_image).cpu()
             bbs = self.detector.decode_output(out, 0.5)
-            # rospy.loginfo(bbs)
-            # if self.count % 40 == 0:
-            #     image1 = ImageDraw.Draw(image)
-            #     for bb in bbs[0]:
-            #         shape = [(bb["x"], bb["y"]), ( bb["x"]+bb["width"], bb["y"]+bb["height"])]
-            #         image1.rectangle(shape, outline ="red")
-            #     image.show()
-
-            # self.count+=1
 
             #publish bb
             for bb in bbs[0]:
+                
+                x = int(bb["x"])
+                y = int(bb["y"])
                 bb_msg = BoundingBox()
                 bb_msg.header.stamp = stamp
                 bb_msg.header.frame_id = frame_id
-                bb_msg.x = bb["x"]
-                bb_msg.y = bb["y"]
+                bb_msg.x = x
+                bb_msg.y = y
                 bb_msg.width = bb["width"]
                 bb_msg.height = bb["height"]
                 bb_msg.category_id = bb["category"]
                 bb_msg.category_name = self.mapping[bb["category"]]
+                
+                
+                
+                start_point = (x, y)
+                end_point = (int(x+bb["width"]), int(y+bb["height"]))
+                color = (255, 0, 0)
+                thickness = 2
+                
+                
+                # visualize image with bb
+                cv_image = cv2.rectangle(cv_image, start_point, end_point, color, thickness)
+                cv_image = cv2.putText(cv_image, self.mapping[bb["category"]], (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 
+                   1, color, thickness, cv2.LINE_AA)
+                imgMsg = self.bridge.cv2_to_imgmsg(cv_image, "rgb8")
+                self.image_bb_pub.publish(imgMsg)
+                
+                
+                
+                
+                # Call function to compute point and depth
+                x,y,depth = self.compute_point(depth_image, bb, depth_image)
+                point = Point()
+                point.x = x
+                point.y = y
+                point.z = depth
+                bb_msg.bb_center = point
                 self.bb_pub.publish(bb_msg)
+                
+              
                 # tinfer = time.time() - t0
                 # rospy.loginfo(tinfer)
 
 
 
+    def compute_point(self, depth, bb, depth_image):
+        depth = depth_image[int(bb["y"]+bb["width"]/2), int(bb["x"]+bb["height"]/2)]
+
+        x=0
+        y=0
+        if self.camera_info is not None:      
+            x = depth*(int(bb["x"]) - self.camera_info[2]) / self.camera_info[0] 
+            y = depth*(int(bb["y"]) - self.camera_info[3]) / self.camera_info[1]
+        
+        return x, y, depth
 
                 
     def run(self):
