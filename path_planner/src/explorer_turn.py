8cfebc7a620d1331b41e6de8816e990ec9dca4ab
Gustav Bernpaintner <bernp@kth.se>
2023-03-24 17:09:41 +0100

    exploration edits

--- path_planner/src/explorer_turn.py
+++ path_planner/src/explorer_turn.py
@@ -3,47 +3,37 @@
 import math
 import rospy
 from geometry_msgs.msg import PoseStamped, Twist
+from rospy import Service
+from std_srvs.srv import Trigger
 
 
-class explorer_turn():
+class ExplorerTurn():
 
     def __init__(self):
 
         #Init node
         rospy.init_node('explorer_turn')
-
         
         #subscribers
         self.map_sub = rospy.Subscriber('/map', PoseStamped, self.map_callback) # change to the topic that the map is published on
-
         
         #publishers
         self.cmd_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
         
         # To control the robots movement
         self.move = Twist()
-        self.max_angle = 0.1
         self.angle_speed = 0.8
-        self.in_goal_tolerance = 0.02
-        
-        self.turn360()
 
+        # Service to explore
+        self.turn360_service = Service('/explore', Trigger, self.turn)
+        rospy.spin()
 
-    def turn360(self):
-
-        angle_rotated = 0
+    def turn(self):
         self.move.angular.z = self.angle_speed
-        
-        while angle_rotated < math.pi*2:
-            self.cmd_pub.publish(self.move)
-            rospy.sleep(0.1)
-            angle_rotated += abs(self.move.angular.z) * 0.1 # need to be tuned to the real robot
-            # print(angle_rotated)
-            
-        self.move.angular.z = 0
         self.cmd_pub.publish(self.move)
+        # self.move.angular.z = 0
+        # self.cmd_pub.publish(self.move)
 
 
 if __name__ == '__main__':
-    explorer = explorer_turn()
-    rospy.spin()
+    explorer = ExplorerTurn()
