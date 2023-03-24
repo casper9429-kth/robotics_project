121c01fa6df74b1ea37109290a5d1ebbdf241765
Gustav Bernpaintner <bernp@kth.se>
2023-03-24 17:14:49 +0100

    merge conflict actually fixed

--- path_planner/src/explorer_turn.py
+++ path_planner/src/explorer_turn.py
@@ -15,19 +15,13 @@ class ExplorerTurn():
         rospy.init_node('explorer_turn')
         
         #subscribers
-<<<<<<< HEAD
         self.map_sub = rospy.Subscriber('/map', PoseStamped, self.map_callback) # change to the topic that the map is published on
-=======
-        # self.map_sub = rospy.Subscriber('/map', PoseStamped, self.map_callback) # change to the topic that the map is published on
-
->>>>>>> 94bf2a08e14e8070f64d8afc9921f3984022c241
         
         #publishers
         self.cmd_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
         
         # To control the robots movement
         self.move = Twist()
-<<<<<<< HEAD
         self.angle_speed = 0.8
 
         # Service to explore
@@ -36,29 +30,6 @@ class ExplorerTurn():
 
     def turn(self):
         self.move.angular.z = self.angle_speed
-=======
-        self.max_angle = 0.1
-        self.angle_speed = math.pi/4 # need to be tuned to the real robot
-        self.in_goal_tolerance = 0.02
-        self.rate = rospy.Rate(10)
-        for i in range(50):
-            self.rate.sleep()
-        self.turn360()
-        
-     
-    def turn360(self):
-
-        angle_rotated = 0
-        self.move.angular.z = self.angle_speed
-        
-        while angle_rotated < math.pi*4:
-            self.cmd_pub.publish(self.move)
-            rospy.sleep(0.1)
-            angle_rotated += abs(self.move.angular.z) * 0.1 # need to be tuned to the real robot
-            print(angle_rotated)
-            
-        self.move.angular.z = 0
->>>>>>> 94bf2a08e14e8070f64d8afc9921f3984022c241
         self.cmd_pub.publish(self.move)
         # self.move.angular.z = 0
         # self.cmd_pub.publish(self.move)
