437614e53bfb1172d25719e6c14f035c71e7a6a9
carmor1337 <erik.a.e.skog@gmail.com>
2023-04-26 15:44:18 +0200

    initial arm_camera_services commit

---
+++ detection/src/arm_camera_services.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import rospy
+from rospy import Service
+
+from std_srvs.srv import Trigger, TriggerResponse
+from rgb_detection import RGBDetection
+
+class ArmCameraServices:
+    def __init__(self):
+        rospy.init_node('arm_camera_services')
+
+        rgb_detection = RGBDetection()
+
+        # Services
+        self.start_service = Service('arm_camera_services/start', Trigger, self.callback)
+        self.stop_service = Service('arm_camera_services/stop', Trigger, self.callback)
+        self.is_running_service = Service('arm_camera_services/is_running', Trigger, self.callback)
+
+    def run(self):
+        rospy.spin()
+
+
+if __name__ == "__main__":
+    node = ArmCameraServices()
+    node.run()
