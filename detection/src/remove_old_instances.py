95829f981a5db505516765c683bf922805356c6b
robot <nolwenn.deschand@insa-lyon.fr>
2023-04-28 14:59:27 +0200

    remove old objects tested and done

--- detection/src/remove_old_instances.py
+++ detection/src/remove_old_instances.py
@@ -14,23 +14,27 @@ class RemoveOldInstances():
         # Publisher
         self.delete_instance_pub = rospy.Publisher("/detection/remove_instance", String, queue_size=10)
 
-        self.expiration_time = 5
+        self.expiration_time = rospy.get_param('expiration_time')
+        self.instance_list = None
 
 
     
     def callback_instances(self, msg):
         """ Callback for the instances topic"""
-        for instance in msg.instances:
+        self.instance_list = msg.instances
+        
+    def remove_old_instances(self):
+        if not self.instance_list is None:
             stamp = rospy.Time.now()
-            if instance.latest_stamp < stamp - rospy.Duration(self.expiration_time):
-                self.delete_instance_pub.publish(String(instance.instance_name))
-
+            for instance in self.instance_list:
+                if instance.latest_stamp < stamp - rospy.Duration(self.expiration_time):
+                    self.delete_instance_pub.publish(String(instance.instance_name))
+    
     def run(self):
-        """
-        Run the node. 
-        Don't change anything here, change main instead.
-        """
-        rospy.spin()
+        while not rospy.is_shutdown():
+            rospy.sleep(0.5)
+            self.remove_old_instances()
+        
 
 
 if __name__ == "__main__":
