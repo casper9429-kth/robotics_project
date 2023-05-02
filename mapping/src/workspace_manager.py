d7438543e2a4193a7d52c0b47f9b6c4100cc6ebc
robot <nolwenn.deschand@insa-lyon.fr>
2023-05-02 16:53:53 +0200

    check inside workspace

--- mapping/src/workspace_manager.py
+++ mapping/src/workspace_manager.py
@@ -2,43 +2,62 @@
 import rospy
 from geometry_msgs.msg import PoseStamped, PoseArray, Pose, Point
 import tf2_ros
-import tf2_geometry_msgs
-from visualization_msgs.msg import Marker, MarkerArray
+import tf2_geometry_msgs # This package has magical side effects, don't remove this import
+from visualization_msgs.msg import Marker
+from rospy import Subscriber, Service, Publisher
+from math import inf
+from mapping.srv import CheckPolygon, CheckPolygonResponse, CheckPolygonRequest
 
 
+class Polygon:
+    def __init__(self, corner_points: PoseArray):
+        self.corner_points = corner_points
 
-class workspace_manager():
+    def is_inside(self,x,y):
+        """Check if point is inside polygon"""
+        corners = [(point.position.x, point.position.y) for point in self.corner_points.poses]
+        n = len(corners)
+        inside = False
+
+        p1x, p1y = corners[0]
+        for i in range(n+1):
+            p2x, p2y = corners[i % n]
+            if y > min(p1y, p2y):
+                if y <= max(p1y, p2y):
+                    if x <= max(p1x, p2x):
+                        if p1y != p2y:
+                            xints = (y - p1y) * (p2x - p1x) / (p2y - p1y) + p1x
+                        if p1x == p2x or x <= xints:
+                            inside = not inside
+            p1x,p1y = p2x,p2y
+
+        return inside
+
+
+class WorkspaceManager:
     def __init__(self) -> None:
-        """ Put the node name here, and description of the node"""
         rospy.init_node('workspace_manager')
-
-        # Subscribers 
-        #self.sub_topic = rospy.Subscriber("topic", type, self.callback_topic)
         
         # Publisher
-        self.message_pub_alt = rospy.Publisher("/geofence/pose_array", PoseArray, queue_size=10)
-        self.message_pub = rospy.Publisher("/workspace_poses/pose_array", PoseArray, queue_size=10)
-        self.marker_pub = rospy.Publisher("/workspace_poses/marker", Marker, queue_size=10)
-        #self.message_pub_point = rospy.Publisher("/workspace_poses/pose", PoseStamped, queue_size=10)
+        self.message_pub_alt = Publisher("/geofence/pose_array", PoseArray, queue_size=10) # TODO: find out why this exists
+        self.message_pub = Publisher("/workspace_poses/pose_array", PoseArray, queue_size=10)
+        self.marker_pub = Publisher("/workspace_poses/marker", Marker, queue_size=10)
+        
+        # Services
+        self.is_inside_service = Service("/workspace/is_inside", CheckPolygon, self.is_inside_callback)
 
         # Define rate
         self.update_rate = 1 # [Hz] Change this to the rate you want
         self.update_dt = 1.0/self.update_rate # [s]
-        self.rate = rospy.Rate(self.update_rate) 
+        self.rate = rospy.Rate(self.update_rate)
         
-
-        # Paramethers HERE
-        self.pose_array = PoseArray()
+        # Parameters
+        self.pose_array = self.read_poses()
+        self.polygon = Polygon(self.pose_array)
         
-    ###### All your callbacks here ######
-        
-    # def callback_topic(self): 
-    #     """Callback function for the topic"""seStamped' object has no attribute 'position'
-
-    #     # do callback stuff
-    #     pass
-
-    ###### All your other methods here #######
+    def is_inside_callback(self, req: CheckPolygonRequest):
+        result = self.polygon.is_inside(req.x, req.y)
+        return CheckPolygonResponse(result)
 
     def is_valid_decimal(self,num):
         try:
@@ -48,8 +67,22 @@ class workspace_manager():
         else:
             return True
         
+    def read_poses(self):
+        pose_array = PoseArray()
+        filepath = '/home/robot/dd2419_ws/src/mapping/src/workspace.tsv'
+        with open(filepath) as file:
+            for line in file:
+                value = line.strip().split('\t')
+                point = Pose()
 
-
+                if self.is_valid_decimal(value[0]) and self.is_valid_decimal(value[1]):
+                    point.position.x = float(value[0])
+                    point.position.y = float(value[1])
+                    pose_array.poses.append(point)
+                
+        pose_array.header.frame_id = 'map'
+        return pose_array
+        
     def visualize_point(self):
         poselist = [(pose.position.x, pose.position.y, pose.position.z) for pose in self.pose_array.poses]
 
@@ -64,9 +97,9 @@ class workspace_manager():
         mark.color.g = 1.0
         mark.color.b = 0.0
         mark.pose.orientation.w = 1.0
-        mark.pose.position.x = 0.0#poselist[0][0]
-        mark.pose.position.y = 0.0#poselist[0][1]
-        mark.pose.position.z = 0.0#poselist[0][2]
+        mark.pose.position.x = 0.0
+        mark.pose.position.y = 0.0
+        mark.pose.position.z = 0.0
 
         mark.id = 0
         mark.lifetime = rospy.Duration(0)
@@ -75,31 +108,16 @@ class workspace_manager():
 
         self.marker_pub.publish(mark)
 
-
     def main(self): # Do main stuff here    
         """
         Main loop, instead of changing run function,
         write your code here to make it more readable.
         """
-        filepath = '/home/robot/dd2419_ws/src/mapping/src/workspace.tsv'
-        with open(filepath) as file:
-            for line in file:
-                value = line.strip().split('\t')
-                #print(value)
-                point = Pose()
-
-                if self.is_valid_decimal(value[0]) and self.is_valid_decimal(value[1]):
-                    point.position.x = float(value[0])
-                    point.position.y = float(value[1])
-                    self.pose_array.poses.append(point)
-                
-        self.pose_array.header.frame_id = 'map'
+        
         self.message_pub.publish(self.pose_array)
         self.message_pub_alt.publish(self.pose_array)
 
         self.visualize_point()
-
-        self.pose_array = PoseArray()
 
     def run(self):
         """
@@ -117,6 +135,5 @@ class workspace_manager():
 
 
 if __name__ == "__main__":
-
-    node=workspace_manager()
+    node = WorkspaceManager()
     node.run()
--- path_planner/src/path_tracker.py
+++ path_planner/src/path_tracker.py
@@ -1,102 +1,14 @@
-from math import atan2, hypot, inf
-
+#!/usr/bin/env python3
+from math import atan2, hypot
 import rospy
 import tf2_geometry_msgs  # this import is needed for tf2_ros.transform to work
 import tf2_ros
-from rospy import Service, Publisher, Subscriber
+from rospy import Service, Publisher, Subscriber, ServiceProxy
 from geometry_msgs.msg import Twist, PoseStamped, PoseArray
 from std_srvs.srv import Trigger, TriggerResponse
 from path_planner.srv import Bool, BoolResponse
-
+from mapping.srv import CheckPolygon, CheckPolygonRequest
 from tf.transformations import euler_from_quaternion
-
-
-class LineSegment:
-    def __init__(self,x1,y1,x2,y2) -> None:
-        self.x1 = x1
-        self.y1 = y1
-        self.x2 = x2
-        self.y2 = y2
-        self.k = self.get_slope()
-        self.m = self.get_y_intercept()
-        self.is_horizontal = False
-        self.is_vertical = False
-    
-    def get_slope(self):
-        try:
-            return (self.y2-self.y1)/(self.x2-self.x1)
-        except ZeroDivisionError:
-            self.is_vertical = True
-            return inf
-
-    def get_y_intercept(self):
-        return self.y1 - self.get_slope()*self.x1
-
-    def get_x_intercept(self,y):
-        try:
-            return (y-self.m)/self.k
-        except ZeroDivisionError:
-            self.is_horizontal = True
-            return inf
-
-    def ray_intersect(self, ray_start):
-        """
-        Returns True if the ray intersects the line segment, False otherwise.
-        The ray starts at ray_start and extends to the right.
-        """
-        if self.is_vertical:
-            return ray_start.x < self.x1
-        elif self.is_horizontal:
-            return False
-        else:
-            is_between_y = not (ray_start.y < self.y1 and ray_start.y < self.y2 or ray_start.y > self.y1 and ray_start.y > self.y2)
-            x_to_the_left = ray_start.x < self.get_x_intercept(ray_start.y)
-            return is_between_y and x_to_the_left
-        
-
-class Polygon:
-    def __init__(self, pose_array: PoseArray):
-        self.segments = self.generate_line_segments(pose_array)
-        self.point_array = [(point.position.x, point.position.y) for point in pose_array] 
-    def contains(self, point):
-        """
-        Returns True if the point is inside the polygon, False otherwise.
-        The point is something that has an x and y attribute, like a Point.
-        """
-        # The point is inside the polygon if an imaginary ray starting at the point
-        # intersects the polygon an odd number of times.
-        return self.is_point_in_polygon(point.x, point.y, self.point_array)
-
-
-    def is_point_in_polygon(self,x,y,poly):
-        """Check if point is in polygon"""
-        n = len(poly)
-        inside =False
-
-        p1x,p1y = poly[0]
-        for i in range(n+1):
-            p2x,p2y = poly[i % n]
-            if y > min(p1y,p2y):
-                if y <= max(p1y,p2y):
-                    if x <= max(p1x,p2x):
-                        if p1y != p2y:
-                            xints = (y-p1y)*(p2x-p1x)/(p2y-p1y)+p1x
-                        if p1x == p2x or x <= xints:
-                            inside = not inside
-            p1x,p1y = p2x,p2y
-
-        return inside
-
-    def generate_line_segments(self, point_array):
-        segments = []
-        point_array.append(point_array[0])
-        for i in range(len(point_array)-1):
-            x1 = point_array[i].position.x
-            y1 = point_array[i].position.y
-            x2 = point_array[i+1].position.x
-            y2 = point_array[i+1].position.y
-            segments.append(LineSegment(x1,y1,x2,y2))
-        return segments
 
 
 class PathTracker:
@@ -118,8 +30,8 @@ class PathTracker:
         
         # State
         self.rate = rospy.Rate(10)
-        self.workspace = None
         self.goal = None
+        self.polygon_response = None
         self.is_running = False
         self.is_observing = False
         self.move_timer = None
@@ -136,11 +48,14 @@ class PathTracker:
         self.stop_service = Service('/path_tracker/stop', Trigger, self.stop_callback)
         self.is_running_service = Service('/path_tracker/is_running', Bool, self.is_running_callback)
         
+        # Service Proxies
+        rospy.wait_for_service("/workspace/is_inside")
+        self.is_inside_workspace = ServiceProxy("/workspace/is_inside", CheckPolygon)
+
         # Publishers
         self.cmd_vel_publisher = Publisher('/cmd_vel', Twist, queue_size=1)
         
         # Subscribers
-        self.workspace_subscriber = Subscriber('/workspace_poses/pose_array', PoseArray, self.workspace_callback)
         self.goal_subscriber = Subscriber('/move_base_simple/goal', PoseStamped, self.goal_callback)
 
     def start_callback(self, req):
@@ -156,18 +71,14 @@ class PathTracker:
 
     def goal_callback(self, msg: PoseStamped):
         self.goal = msg
-        # self.goal.header.stamp = rospy.Time(0) # always use the latest transform
         self.goal.header.frame_id = 'map'
-
-    def workspace_callback(self, msg: PoseArray):
-        self.workspace = Polygon(msg.poses)
 
     def calculate_cmd_twist(self):
         goal_in_base_link = None
         try:
-            goal_in_base_link = self.buffer.transform(self.goal, 'base_link', rospy.Duration(0.5))
+            goal_in_base_link = self.buffer.transform(self.goal, 'base_link')
         except: # TODO: catch only relevant exceptions
-            #print(self.buffer.can_transform('base_link', 'map', rospy.Time(0)))
+            # print(self.buffer.can_transform('base_link', 'map', rospy.Time(0)))
             return Twist()
         
         angle_to_goal = atan2(goal_in_base_link.pose.position.y, goal_in_base_link.pose.position.x)
@@ -198,10 +109,7 @@ class PathTracker:
                 # TODO: publish a message that the goal has been reached
         
         return cmd_twist
-    
-    def goal_in_workspace(self):
-        return self.workspace and self.goal and self.workspace.contains(self.goal.pose.position)
-    
+
     def start(self):
         self.is_running = True
         self.move_timer = rospy.Timer(rospy.Duration(self.move_duration),
@@ -230,9 +138,12 @@ class PathTracker:
     
     def run(self):
         while not rospy.is_shutdown():
-            if self.is_running and self.goal_in_workspace(): #TODO: workspace is buggy
-                cmd_twist = Twist() if self.is_observing else self.calculate_cmd_twist()
-                self.cmd_vel_publisher.publish(cmd_twist)
+            
+            if self.goal and self.is_running:
+                goal_point = self.goal.pose.position.x, self.goal.pose.position.y
+                if self.is_inside_workspace(*goal_point):
+                    cmd_twist = Twist() if self.is_observing else self.calculate_cmd_twist()
+                    self.cmd_vel_publisher.publish(cmd_twist)
             self.rate.sleep()
             
           
